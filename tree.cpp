fd43170831b7f720657cd44dcd5e1dfc5956be13
Aditya Uday Ubale <51261116+aditya25022001@users.noreply.github.com>
2021-01-04 10:57:21 +0530

    Traversals

--- tree.cpp
+++ tree.cpp
@@ -6,10 +6,27 @@ struct node{
     node *right;
 };
 node *root = NULL;
-void preOrderTraversal(){}
-void postOrderTraversal(){}
+int c=0;
+void preOrderTraversal(node *t){
+    if(t==NULL)
+        return;    
+    cout<<"->"<<t->data;
+    preOrderTraversal(t->left);
+    preOrderTraversal(t->right);
+}
+void postOrderTraversal(node *t){
+    if(t==NULL)
+        return;   
+    postOrderTraversal(t->left);
+    postOrderTraversal(t->right);
+    cout<<"->"<<t->data;
+}
 void inOrderTraversal(node *t){
-    
+    if(t==NULL)
+        return;
+    inOrderTraversal(t->left);
+    cout<<"->"<<t->data;
+    inOrderTraversal(t->right);
 }
 int inOrderSuccessor(int dataI){
     node *temp = root;
@@ -35,16 +52,23 @@ void addLeaf(int dataI){
         root=temp;
     else{
         node *tmp = root;
+        node *tempPrev;
         while(tmp!=NULL){
             if(temp->data<tmp->data){
-                cout<<temp->data<<" "; 
+                tempPrev=tmp;
                 tmp=tmp->left;
+                c=0;
             }
             else{
+                tempPrev=tmp;
                 tmp=tmp->right;
+                c=1;
             }
         }
-        tmp=temp;
+        if(c==0)
+            tempPrev->left=temp;
+        if(c==1)
+            tempPrev->right=temp;
     }
 }
 int main(){
@@ -56,5 +80,10 @@ int main(){
     addLeaf(6);
     addLeaf(8);
     addLeaf(9);
+    cout<<endl<<"Inorder ";
     inOrderTraversal(root);
+    cout<<endl<<"Preorder ";
+    preOrderTraversal(root);
+    cout<<endl<<"Postorder ";
+    postOrderTraversal(root);
 }
