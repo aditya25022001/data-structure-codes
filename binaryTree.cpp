588b6043baea19285f80884c813054376df26da9
Aditya Uday Ubale <51261116+aditya25022001@users.noreply.github.com>
2021-01-07 21:59:14 +0530

    final

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -1,55 +1,58 @@
 #include<iostream>
 using namespace std;
 struct node{
-    char data;
+    string data;
     node *left;
     node *right;
 };
-char treeLeaves[100];
-node *parent;
+int index=0;
+string treeLeaves[100];
 node *root=NULL;
-void addLeaf(char a, char b){
-    node *temp = new node;
-    temp->data=a;
-    temp->right=NULL;
-    temp->left=NULL;
-    if(root==NULL)
-        root=temp;
-    if(b=='p')
-        parent=temp;
-    if(b=='s')
-        parent->left=temp;
-    if(b=='o'){
-        parent->right=temp;
-        node *tmp=root;
-        while(tmp->left!=parent || tmp->right!=parent){
-            
-        }
-    }
-}
+node *parents[100];
 void createTree(int n){
-    for(int i=1;i<n;++i){
-        if(treeLeaves[i+1]=='('){
-            addLeaf(treeLeaves[i], 'p');
-            i++;
-        }       
-        if(treeLeaves[i+1]==','){
-            addLeaf(treeLeaves[i], 's');
-            i++;
-        } 
-        if(treeLeaves[i+1]==')'){
-            addLeaf(treeLeaves[i], 'o');
-            i++;
+    for(int i=0;i<n;i++){
+        if(treeLeaves[i]!="(" && treeLeaves[i]!="," && treeLeaves[i]!=")"){
+            node *temp = new node;
+            temp->data = treeLeaves[i];
+            temp->right=NULL;
+            temp->left=NULL;
+            if(treeLeaves[i-1]=="(" && treeLeaves[i+1]=="(" && root!=NULL){
+                parents[index-1]->left=temp;
+                parents[index]=temp;
+                index++;
+            }
+            if(treeLeaves[i-1]=="," && treeLeaves[i+1]=="(" && root!=NULL){
+                parents[index-1]->right=temp;
+                parents[index]=temp;
+                index++;
+            }
+            if(treeLeaves[i-1]=="," && treeLeaves[i+1]==")" && root!=NULL){
+                parents[index-1]->right=temp;
+                index--;
+            }
+            if(treeLeaves[i-1]=="(" && treeLeaves[i+1]=="," && root!=NULL){
+                parents[index-1]->left=temp;
+            }
+            if(treeLeaves[i-1]=="(" && treeLeaves[i+1]==")" && root!=NULL){
+                parents[index-1]->left=temp;
+                index--;
+            }
+            if(root==NULL){
+                root=temp;
+                parents[index]=temp;
+                index++;
+            }
         }
-        if(treeLeaves[i]==')' || treeLeaves[i]=='(' || treeLeaves[i]==',');
     }
 }
 void traverseInOrderRecursion(node *t){
     if(t==NULL)
         return;
     traverseInOrderRecursion(t->left);
+    cout<<"->"<<t->data;
     traverseInOrderRecursion(t->right);
 }
+/*
 void traversePreOrderRecursion(){
     
 }
@@ -64,7 +67,7 @@ void traversePreOrderIterative(){
 }
 void traversePostOrderIterative(){
     
-}
+}*/
 int main(){
     int n;
     cout<<endl<<"Enter tree string size with parenthesis and comma : ";
@@ -72,5 +75,7 @@ int main(){
     cout<<endl<<"Enter tree string one by one with parenthesis and comma : ";
     for(int i=0;i<n;i++)
         cin>>treeLeaves[i];
+    createTree(n);
+    traverseInOrderRecursion(root);
     return 0;
 }
